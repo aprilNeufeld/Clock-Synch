83e776c080f49ad6563ce2c65ba43b997d94bac7
tricksterCodess <tricksterCodess@users.noreply.github.com>
2021-05-13 15:07:29 -0500

    Remove _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,15 +8,6 @@ import DateFnsUtils from '@date-io/date-fns';
 
 const ClockApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 
-	// Necessary to be able to use Material-UI with NextJS.
-	React.useEffect(() => {
-		// Remove the server-side injected CSS.
-		const jssStyles = document.querySelector('#jss-server-side');
-		if (jssStyles && jssStyles.parentElement) {
-			jssStyles.parentElement.removeChild(jssStyles);
-		}
-	}, [])
-
 	return (
 		<MuiPickersUtilsProvider utils={DateFnsUtils}>
 			<ThemeProvider theme={theme}>
